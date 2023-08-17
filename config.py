2a86bed817541de6ad2b4c69306b9db586a40595
Dinobonecrash1 <128608588+Dinobonecrash1@users.noreply.github.com>
2023-08-17 09:35:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,8 +25,8 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Dinobonecrash1/music
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/guardians_Bot_Updates")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Anime_Chat_Guardians")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
